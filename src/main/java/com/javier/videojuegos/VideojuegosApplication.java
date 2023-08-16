5a48d9f21c966f31f5c5a87898424130e7642d45
Javier Castro <javiercastro27@outlook.es>
2023-08-16 17:54:24 -0600

    Create Service of games 🕹

--- src/main/java/com/javier/videojuegos/VideojuegosApplication.java
+++ src/main/java/com/javier/videojuegos/VideojuegosApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VideojuegosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideojuegosApplication.class, args);
+            SpringApplication.run(VideojuegosApplication.class, args);
 	}
 
 }
--- src/main/java/com/javier/videojuegos/controller/ListadoController.java
+++ src/main/java/com/javier/videojuegos/controller/ListadoController.java
@@ -1,14 +1,30 @@
 package com.javier.videojuegos.controller;
 
+import com.javier.videojuegos.domain.Videojuego;
+import com.javier.videojuegos.service.VideojuegoService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ListadoController {
-    
+
+    private final VideojuegoService videojuegoService;
+
+    public ListadoController(VideojuegoService videojuegoService) {        
+        this.videojuegoService = videojuegoService;
+    }
+        
     @RequestMapping("/")
-    public String listarVideojuegos (){
+    public String listarVideojuegos (Model model){  
+        
+        // Invocamos nuestro Servicio
+        List<Videojuego> destacados = videojuegoService.buscarDestacados();
+        
         // Busqueda de los videojuegos
+        model.addAttribute("videojuegos",destacados);
+        
         return"listado";
     }
     
